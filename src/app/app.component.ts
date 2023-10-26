01febb1852b06f88c3a16e09d7495fadae0f2630
Irakli <iraklichabiev7@gmail.com>
2023-10-26 14:46:23 +0400

    initialcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { forbiddenNameValidator } from './user-name.validator';
+import { PasswordValidator } from './password.validator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'forms';
+export class AppComponent implements OnInit{
+
+  registrationForm!: FormGroup;
+
+  get userName(){
+    return this.registrationForm.get('userName');
+  }
+
+  get email(){
+    return this.registrationForm.get('email')
+  }
+
+  get alternateEmails(){
+    return this.registrationForm.get('alternateEmails') as FormArray;
+  }
+
+  addAlternateEmail(){
+    this.alternateEmails.push(this.fb.control(''));
+  }
+
+
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/)]],
+      email: [''],
+      subscribe: [false],
+      password: [''],
+      confirmPassword: [''],
+      address: this.fb.group({
+        city: [''],
+        state: [''],
+        postalCode: ['']
+      }),
+      alternateEmails: this.fb.array([])
+    },
+      {validator: PasswordValidator});
+
+    this.registrationForm.get('subscribe')?.valueChanges
+    .subscribe(checkedValue =>{
+      const email = this.registrationForm.get('email');
+      if(checkedValue){
+        email?.setValidators(Validators.required);
+      } else {
+        email?.clearValidators();
+      }
+      email?.updateValueAndValidity
+    })
+  }
+
+  
+
+
 }
